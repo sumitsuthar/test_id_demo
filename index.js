717a22920c245cac089adfdbad51c8d9c28a195d
Sumit Suthar <ssuthar@newrelic.com>
2024-10-21 19:17:51 +0530

    rce api added

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
+app.get("/rce/attack", (req, res) => {
+    var payload = req.query["payload"];
+    var cmd = payload;
+    cp.exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.send(stdout.toString());
+    });
+  });
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
