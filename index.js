c9100d931b3ef1f08b0d4e8d6f08ffa631c53e5d
Sumit Suthar <ssuthar@newrelic.com>
2024-10-21 17:53:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const newrelic = require('newrelic');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
